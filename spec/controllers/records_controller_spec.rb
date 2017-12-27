fe16949cd036add35e66790ff980f237c34185a6
sagar <sagarbhandari1000@gmail.com>
2017-12-27 14:39:28 +0545

    create action tested

--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -16,4 +16,27 @@ describe "GET #new" do
   end
 end
 
+describe "POST #create" do
+  context "with valid attributes" do
+    it "creates a new record" do
+    	expect {
+    post :create, params: {record: FactoryGirl.attributes_for(:record) }
+    }.to change(Record, :count).by(1)
+
+    end
+    it "redirects to root_path" do
+
+    end
+  end
+
+  context "with invalid attributes" do
+    it "doesn't create a new record" do
+
+    end
+    it "renders the new template" do
+
+    end
+  end
+end
+
 end
---
+++ spec/factories/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :record do
+    title "Record Title"
+    date  "2017-01-03"
+    amount "25000"
+  end
+end
